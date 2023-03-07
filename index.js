74528175e903ef2caaddd12afb2e919e113547c0
Vikash Kumar Mishra <70460666+vikashmishra-codes@users.noreply.github.com>
2023-03-07 21:47:58 +0530

    Adding files

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`server is running on port: ${PORT}`)
+        )
+    )
+    .catch((err) => console.log(err.message));
