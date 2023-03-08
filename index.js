1d18ca737772aed8fdf24e3a1fa94f7fda22cc37
Vikash Kumar Mishra <70460666+vikashmishra-codes@users.noreply.github.com>
2023-03-08 19:35:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-
+app.get("/", (req, res) => {
+    res.send("Hello to memory Api");
+});
 const PORT = process.env.PORT || 5000;
 
 mongoose
